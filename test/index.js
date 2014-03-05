7f5f75d60bc1cf51ba86e70e700f0e703c128cad
Hernán Tierno <htierno@gmail.com>
2014-03-05 18:10:44 -0300

    New Tests

--- test/index.js
+++ test/index.js
@@ -10,6 +10,20 @@ describe("Chapter 6 - Arrays (pseudo arrays)", function () {
     it("length property weird behavior", function () {
         var arr = ["one", "two"];
         arr[500] = "three";
-        console.log("length should be 3 but is: " + arr.length);
+        assert.equal(arr.length, 501);
+        //console.log("length should be 3 but is: " + arr.length); //501
+    });
+    it("delete an element (The wrong way)", function () {
+        var arr = ["one", "two"];
+        delete arr[1];
+        assert.equal(arr.length, 2);
+        //console.log("length should be 1 but is: " + arr.length); //2
+    });
+    it("delete an element (The right way)", function () {
+        var arr = ["one", "two", "three"];
+        arr.splice(2, 1); //Removes one element from index 2 (should delete 'three')
+        assert.equal(arr.length, 2);
+        assert.equal(arr[0], "one");
+        assert.equal(arr[1], "two");
     });
 });
